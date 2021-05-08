9626dffd058d27e1613c31f885fb59ad63475844
Jan Piotrowski <piotrowski+github@gmail.com>
2021-05-08 00:27:27 +0000

    remove nexus-plugin-prisma

--- generated/nexus-typegen.ts
+++ generated/nexus-typegen.ts
@@ -7,12 +7,7 @@
 import { Context } from "./../graphql/context"
 
 
-declare global {
-  interface NexusGenCustomOutputProperties<TypeName extends string> {
-    model: NexusPrisma<TypeName, 'model'>
-    crud: any
-  }
-}
+
 
 declare global {
   interface NexusGen extends NexusGenTypes {}
--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -1,10 +1,20 @@
-import { arg, inputObjectType, mutationType, objectType, queryType } from 'nexus'
+import { objectType } from 'nexus'
 
+/*
 export const User = objectType({
   name: 'User',
 
   definition(t) {
     t.model.id()
     t.model.name()
+  }
+})
+*/
+
+export const User = objectType({
+  name: "User",
+  definition(t) {
+    t.nonNull.int("id")
+    t.nonNull.string("name")
   }
 })
